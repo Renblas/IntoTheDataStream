4249492285186f80061eb9765dd147405a2d2519
GibbonsGearheads <230198@cghsfl.org>
2023-01-10 17:27:53 -0500

    first draft of collision

--- js/class/player.js
+++ js/class/player.js
@@ -30,7 +30,7 @@ class Player {
         cameraObj.drawImg(this.sprite, this.pos, this.size);
     }
     update() {
-        this.move();
+        this.move(false);
         this.detectColision();
 
         try {
@@ -46,9 +46,14 @@ class Player {
             }
         }
     }
-    move() {
-        this.pos.x += inputManager.playerMoveVec.x * this.moveSpeed * deltaTimeFixed;
-        this.pos.y += inputManager.playerMoveVec.y * this.moveSpeed * deltaTimeFixed;
+    move(reversed) {
+        if (!reversed) {
+            this.pos.x += inputManager.playerMoveVec.x * this.moveSpeed * deltaTimeFixed;
+            this.pos.y += inputManager.playerMoveVec.y * this.moveSpeed * deltaTimeFixed;
+        } else {
+            this.pos.x += inputManager.playerMoveVec.x * this.moveSpeed * deltaTimeFixed * -1;
+            this.pos.y += inputManager.playerMoveVec.y * this.moveSpeed * deltaTimeFixed * -1;
+        }
     }
     fireBullet() {
         var config = JSON.parse(JSON.stringify(this.standaredBullet));
@@ -73,9 +78,27 @@ class Player {
                         var tileSize = world.getTile(b, i).size;
                         collidedWall = collideRectRect(this.pos.x - (this.size.x / 2), this.pos.y - (this.size.y / 2), this.size.x, this.size.y, b - (tileSize.x / 2), i - (tileSize.y / 2), tileSize.x, tileSize.y);
 
+                        if (collidedWall) {
+                            if (this.pos.x - (this.size.x / 2) < b - (tileSize.x / 2)) {
+                                this.pos.x += inputManager.playerMoveVec.x * this.moveSpeed * deltaTimeFixed * -1;
+                            }
+
+                            if (this.pos.x + (this.size.x) > b + (tileSize.x)) {
+                                this.pos.x += inputManager.playerMoveVec.x * this.moveSpeed * deltaTimeFixed * -1;
+                            }
+
+                            if (this.pos.y - (this.size.y / 2) < b - (tileSize.y / 2)) {
+                                this.pos.y += inputManager.playerMoveVec.y * this.moveSpeed * deltaTimeFixed * -1;
+                            }
+
+                            if (this.pos.y + (this.size.y) > b + (tileSize.y)) {
+                                this.pos.y += inputManager.playerMoveVec.y * this.moveSpeed * deltaTimeFixed * -1;
+                            }
+                        }
+
                     } else { collidedWall = false; }
                     if (collidedWall) { hitCounter += 1; }
-                    //if (hitCounter > 0) { this.sprite.img = "enemy"; } else { this.sprite.img = "player"; }
+                    if (hitCounter > 0) { this.sprite.img = "enemy"; } else { this.sprite.img = "player"; }
                 } catch (e) { }
             }
         }
