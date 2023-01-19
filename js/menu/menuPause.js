6c47abe7779343a90c3f64a6b0843019075380d6
Nick-Admin <118198007+Nick-Admin@users.noreply.github.com>
2023-01-19 14:34:54 -0500

    yoooo

--- js/menu/menuPause.js
+++ js/menu/menuPause.js
@@ -8,54 +8,57 @@
  */
 
 var menu__Pause = {
-    initialized: false,
+  initialized: false,
 
-    init: function () {
-        this.buttonReset = new Clickable();
-        this.buttonReset.resize(50 * canvasSize.x, 50 * canvasSize.y);
-        this.buttonReset.locate(width / 2 - this.buttonReset.width / 2, height * 0.25 - this.buttonReset.height / 2);
-        this.buttonReset.color = "#DC143C"; //Background color of the clickable (hex number as a string)
-        this.buttonReset.cornerRadius = 10; //Corner radius of the clickable (float)
-        this.buttonReset.strokeWeight = 2; //Stroke width of the clickable (float)
-        this.buttonReset.stroke = "#000000"; //Border color of the clickable (hex number as a string)
-        this.buttonReset.text = "Reset"; //Text of the clickable (string)
-        this.buttonReset.textColor = "#000000"; //Color of the text (hex number as a string)
-        this.buttonReset.textSize = 22 * canvasSize.y; //Size of the text (integer)
-        this.buttonReset.onPress = function () {
-            menuState = "main";
-            inGame = false;
-        }
+  init: function () {
+    this.buttonReset = new Clickable();
+    this.buttonReset.resize(50 * canvasSize.x, 50 * canvasSize.y);
+    this.buttonReset.locate(
+      width / 2 - this.buttonReset.width / 2,
+      height * 0.25 - this.buttonReset.height / 2
+    );
+    this.buttonReset.color = "#DC143C"; //Background color of the clickable (hex number as a string)
+    //this.buttonReset.alpha = 50;
+    this.buttonReset.cornerRadius = 10; //Corner radius of the clickable (float)
+    this.buttonReset.strokeWeight = 2; //Stroke width of the clickable (float)
+    this.buttonReset.stroke = "#000000"; //Border color of the clickable (hex number as a string)
+    this.buttonReset.text = "Reset"; //Text of the clickable (string)
+    this.buttonReset.textColor = "#000000"; //Color of the text (hex number as a string)
+    this.buttonReset.textSize = 22 * canvasSize.y; //Size of the text (integer)
+    this.buttonReset.onPress = function () {
+      menuState = "main";
+      inGame = false;
+    };
 
-        this.initialized = true;
-    },
+    this.initialized = true;
+  },
 
-    draw: function () {
-        // init if not run before
-        if (!this.initialized) this.init();
+  draw: function () {
+    // init if not run before
+    if (!this.initialized) this.init();
 
-        // draw menu
+    // draw menu
 
-        fill(175, 150, 150, 100);
-        rectMode(CORNER)
-        rect(0, 0, width, height);
+    fill(175, 150, 150, 100);
+    rectMode(CORNER);
+    rect(0, 0, width, height);
 
+    fill(150, 175, 150, 200);
+    rectMode(CENTER);
+    rect(width / 2, height / 2, 200 * canvasSize.x, 64 * canvasSize.y);
 
-        fill(150, 175, 150, 200);
-        rectMode(CENTER)
-        rect(width / 2, height / 2, 200 * canvasSize.x, 64 * canvasSize.y);
+    fill(0);
+    textAlign(CENTER, CENTER);
+    textSize(48 * canvasSize.y);
+    text("Paused", width / 2, height / 2);
 
-        fill(0);
-        textAlign(CENTER, CENTER);
-        textSize(48 * canvasSize.y);
-        text("Paused", width / 2, height / 2);
-
-        rectMode(CORNER);
-        this.buttonReset.textSize = 22 * canvasSize.y; //Size of the text (integer)
-        this.buttonReset.resize(100 * canvasSize.x, 50 * canvasSize.y);
-        this.buttonReset.locate(
-            width / 2 - this.buttonReset.width / 2,
-            height * .3 - this.buttonReset.height / 2
-        );
-        this.buttonReset.draw();
-    },
+    rectMode(CORNER);
+    this.buttonReset.textSize = 22 * canvasSize.y; //Size of the text (integer)
+    this.buttonReset.resize(100 * canvasSize.x, 50 * canvasSize.y);
+    this.buttonReset.locate(
+      width / 2 - this.buttonReset.width / 2,
+      height * 0.3 - this.buttonReset.height / 2
+    );
+    this.buttonReset.draw();
+  },
 };
